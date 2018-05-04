f61803e44fe5bf8aba112dba365ba04a1d1fab9d
Alexis <anestrada95@gmail.com>
2018-05-04 12:09:58 -0700

    start of services page

---
+++ services.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Services</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+        <!-- Bootstrap 3 Lumen -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha384-gv0oNvwnqzF6ULI9TVsSmnULNb3zasNysvWwfT/s4l8k5I+g6oFz9dye0wg3rQ2Q" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    
+    <!-- top area -->
+    <section>
+        <div class="container-fluid indigo">
+          <div class="row">
+            <div class="col-md-3">
+              <img src="img/imageIcon.png" alt="Smart Decisions logo">
+            </div>
+            <div>
+  
+            </div>
+          </div>
+        </div>
+      </section>  
+
+    <!-- navigation bar -->
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+          </div>
+      
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Link</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Link</a></li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+    
+    <!-- services: eBook-->
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5">
+            <img src="img/imageIcon.png" alt="">
+          </div>
+          <div class="col-md-7">
+            <h2>7 Principles: Turn Your Salary into Millions</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Neque voluptates temporibus repellendus sequi ratione consectetur 
+              libero necessitatibus ipsa! At quod consectetur rem impedit dignissimos, 
+              atque facilis suscipit eveniet nulla! Eligendi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- services: seminar -->
+    <section>
+        <div class="container-fluid indigo">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-5">
+                <img src="img/imageIcon.png" alt="">
+              </div>
+              <div class="col-md-7">
+                <h2>Seminar</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                  Neque voluptates temporibus repellendus sequi ratione consectetur
+                  libero necessitatibus ipsa! At quod consectetur rem impedit dignissimos, 
+                  atque facilis suscipit eveniet nulla! Eligendi.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- services: personal coaching -->
+      <section>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-5">
+                <img src="img/imageIcon.png" alt="">
+              </div>
+              <div class="col-md-7">
+                <h2>Personal Coaching</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                  Neque voluptates temporibus repellendus sequi ratione consectetur 
+                  libero necessitatibus ipsa! At quod consectetur rem impedit dignissimos, 
+                  atque facilis suscipit eveniet nulla! Eligendi.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <!-- services: ask Judy -->
+        <section>
+            <div class="container-fluid indigo">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-5">
+                    <img src="img/imageIcon.png" alt="">
+                  </div>
+                  <div class="col-md-7">
+                    <h2>Ask Judy</h2>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                      Neque voluptates temporibus repellendus sequi ratione consectetur
+                      libero necessitatibus ipsa! At quod consectetur rem impedit dignissimos, 
+                      atque facilis suscipit eveniet nulla! Eligendi.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <!-- services: free quote -->
+          <section>
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-5">
+                    <img src="img/imageIcon.png" alt="">
+                  </div>
+                  <div class="col-md-7">
+                    <h2>Personal Coaching</h2>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                      Neque voluptates temporibus repellendus sequi ratione consectetur 
+                      libero necessitatibus ipsa! At quod consectetur rem impedit dignissimos, 
+                      atque facilis suscipit eveniet nulla! Eligendi.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <!-- footer -->
+            <section>
+                <div class="footer">
+                  <div class="container-fluid indigo">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-md-1">
+                            <a class="navbar-brand" href="#">Logo</a>
+                        </div>
+                        <div>
+          
+                        </div>
+              
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+    
+        <!-- jQuery CDN -->
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.js"
+    integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+    crossorigin="anonymous">
+    </script>
+    <!-- javascript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
