cf3b5e4a02e3a7cf54f036f91b8e013b97737700
Alexis <anestrada95@gmail.com>
2018-05-04 11:48:57 -0700

    Start of homepage

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap 3 Lumen -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha384-gv0oNvwnqzF6ULI9TVsSmnULNb3zasNysvWwfT/s4l8k5I+g6oFz9dye0wg3rQ2Q" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Smart Decisions</title>
+</head>
+<body>
+    <!-- top area -->
+    <section>
+      <div class="container-fluid indigo">
+        <div class="row">
+          <div class="col-md-3">
+            <img src="img/imageIcon.png" alt="Smart Decisions logo">
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <!-- navigation bar -->
+    <nav class="navbar navbar-inverse">
+      <!-- navigation -->
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+    
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <a href="index.html">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li>
+              <a href="about.html">About</a>
+            </li>
+            <li>
+              <a href="services.html">Services</a>
+            </li>
+            <li>
+              <a href="references.html">References</a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown
+                <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu" role="menu">
+                <li>
+                  <a href="#">Action</a>
+                </li>
+                <li>
+                  <a href="#">Another action</a>
+                </li>
+                <li>
+                  <a href="#">Something else here</a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                  <a href="#">Separated link</a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                  <a href="#">One more separated link</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-right" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- main image -->
+    <section>
+        <div class="container">
+          <div class="jumbotron">
+            <img src="img/imageIcon.png" alt="large image of Judith">
+          </div>
+        </div>
+    </section>
+
+    <!-- services icons -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="service col-md-3 col-sm-6">
+                  <img src="img/imageIcon.png" alt="image icon" class="img-responsive">
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Voluptate dolore mollitia numquam sit commodi at architecto 
+                     quo iste iure sint! Culpa accusantium veritatis ad consequatur 
+                     officiis rerum vero, dicta tempora?</p>
+                </div>
+                <div class="service col-md-3 col-sm-6">
+                  <img src="img/imageIcon.png" alt="image icon" class="img-responsive">
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                     At assumenda, quia veritatis similique consectetur harum placeat 
+                     qui dicta, mollitia obcaecati ipsum aliquam soluta culpa saepe 
+                     est totam distinctio blanditiis adipisci!</p>
+                </div>
+                <div class="service col-md-3 col-sm-6">
+                  <img src="img/imageIcon.png" alt="image icon" class="img-responsive">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                     In quidem error, nesciunt sed temporibus, quae laborum aliquid 
+                     animi veritatis necessitatibus, harum consequatur! Aut optio 
+                     nobis laudantium ullam obcaecati, nam voluptatibus.</p>
+                </div>
+                <div class="service col-md-3 col-sm-6">
+                  <img src="img/imageIcon.png" alt="image icon" class="img-responsive">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                     Maiores odio similique consequatur praesentium impedit et 
+                     dolorum natus ad iste! A reiciendis nesciunt maxime repellendus 
+                     et omnis sed architecto quo qui.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About -->
+    <section>
+      <div class="container-fluid indigo">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7 about">
+              <h3>My Story</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ut quibusdam assumenda, deleniti nostrum neque ipsa
+                quasi in deserunt sapiente quidem, incidunt dicta commodi ea inventore ducimus? Labore, neque ea.</p>
+              <a href="#">
+                <button class="btn btn-default">More about me</button>
+              </a>
+            </div>
+            <div class="col-md-5">
+              <img src="img/imageIcon.png" alt="photo of Judith" class="img-rounded img-responsive">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- social media -->
+    <section>
+      <div class="container">
+        <h2 class="text-center">Spread the Word!</h2>
+        <img src="img/imageIcon.png" alt="Social1" class="socialIcon">
+        <img src="img/imageIcon.png" alt="Social2" class="socialIcon">
+        <img src="img/imageIcon.png" alt="Social3" class="socialIcon">
+        <img src="img/imageIcon.png" alt="Social4" class="socialIcon">
+        <img src="img/imageIcon.png" alt="Social5" class="socialIcon">
+        </div>
+      </div>
+    </section>
+
+    <!-- footer -->
+    <section>
+      <div class="footer">
+        <div class="container-fluid indigo">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-1">
+                  <a class="navbar-brand" href="#">Logo</a>
+              </div>
+              <div>
+
+              </div>
+    
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <!-- jQuery CDN -->
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous">
+    </script>
+    <!-- javascript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
